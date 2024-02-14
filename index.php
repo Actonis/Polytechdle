55a2456ce437eaf6b52c4761c81671e4c7841b70
Katoche <k.jagueneau@gmail.com>
2024-02-14 10:50:40 +0100

    changements style première page

--- index.php
+++ index.php
@@ -5,13 +5,15 @@ require_once('include/config.php');
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Polytechdle</title>
-    <link rel="icon" href="http://example.com/favicon.png">
+    <title>Polydle</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="https://files.u-angers.fr/data/f-8585d7373e4eb272.ico">
 </head>
 <body>
-    <h1>Welcome to My Website</h1>
-    <p>Thank you for visiting!</p>
+    <div class="container">
+        <img src="https://files.u-angers.fr/data/f-5606dce44e2af1ab.png" alt="Title"  class="img-title">
+    </div>
+    <p>Le jeu journalier par et pour Polytech !</p>
     <div class="center">
         <button class="boutons" onclick="window.location.href = './guess.html';">Classique</button>
         <button class="boutons" onclick="window.location.href = '/services.html';">A faire</button>
