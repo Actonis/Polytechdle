14ef9aa00175f061ee66d0cc9d67911eebce0baf
Actonis <mathis.nagmar@gmail.com>
2024-02-20 18:48:17 +0100

    Ajout d'une liste de choix pour le guess, sera surement à modifier pour le rendre plus pratique

--- js/classique.js
+++ js/classique.js
@@ -1,5 +1,33 @@
 window.onload = function () {
 
+    fetch('http://localhost/Projet/Polytechdle/php/verify.php', {
+            method: 'GET',
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Server response:', data);  
+
+            populateDatalist(data.names);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+        function populateDatalist(names) {
+            var datalist = document.getElementById("choix");
+    
+            // Clear existing options
+            datalist.innerHTML = '';
+    
+            // Populate datalist with names from the database
+            names.forEach(function(name) {
+                var option = document.createElement('option');
+                option.value = name; // Assuming 'name' is the value to display
+                datalist.appendChild(option);
+            });
+        }
+
+
     var input = document.getElementById("guess");
 
     input.addEventListener("keydown", function (e) {
--- php/verify.php
+++ php/verify.php
@@ -1,8 +1,23 @@
 <?php
 require_once('../include/config.php');
 
+// Check if the request is a GET request
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $stmtCheck = $conn->prepare('SELECT nom FROM Professeur');
+    $stmtCheck->execute();
+
+    $names = $stmtCheck->fetchAll(PDO::FETCH_COLUMN);
+    
+    if (!empty($names)) {
+        echo json_encode(['names' => $names]);
+    } else {
+        echo json_encode(['error' => 'No names found']);
+    }
+    
+} 
 // Check if the request is a POST request
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve data from the POST request
     $nom = $_POST['nom'];
 
@@ -12,15 +27,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if( $stmtCheck->rowCount() > 0) {
         echo json_encode(['reponse' => 'Bien joué']);
-        return;
     }
     else {
         echo json_encode(['reponse' => 'Mauvais']);
-        return;
     }
-    
-} else {
-    // Respond with an error message if it's not a POST request
+} 
+else {
+    // Respond with an error message if it's not a GET or POST request
     echo json_encode(['error' => 'Invalid request method']);
 }
 ?>
