13f467a98f78aee2b05c8de445c12894cb41f698
Actonis <mathis.nagmar@gmail.com>
2024-02-20 18:23:23 +0100

    Correction d'erreurs

--- php/verify.php
+++ php/verify.php
@@ -4,16 +4,20 @@ require_once('../include/config.php');
 // Check if the request is a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve data from the POST request
-    $name = $_POST['name'];
+    $nom = $_POST['nom'];
 
     $stmtCheck = $conn->prepare('SELECT * FROM Professeur WHERE nom = :nom');
     $stmtCheck->bindParam(':nom', $nom);
     $stmtCheck->execute();
 
-    
-
-    // Respond with a JSON success message or any necessary data
-    echo json_encode(['success' => true]);
+    if( $stmtCheck->rowCount() > 0) {
+        echo json_encode(['reponse' => 'Bien joué']);
+        return;
+    }
+    else {
+        echo json_encode(['reponse' => 'Mauvais']);
+        return;
+    }
     
 } else {
     // Respond with an error message if it's not a POST request
